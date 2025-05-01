edaabb87f78c6242f81084c5fb56f3dd6bf01b99
Vansh Singhal <vansh.singhal@hotmail.com>
2025-05-01 19:32:06 +0530

    ignore eslint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['assets.aceternity.com','imgs.search.brave.com','images.unsplash.com'],
   },
